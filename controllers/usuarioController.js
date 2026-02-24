125d73d2d5549f0bbd770340454526713b560cec
Aylin Esteban Luna <240853@utxicotepec.edu.mx>
2026-02-24 08:48:06 -0600

    PC-BienesRaices
    add: Se agrego la seccion 04

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,16 +1,13 @@
 const formularioLogin = (req, res) => {
-     res.render("auth/login", {pagina: "Inicia sesión"});
+    res.render('auth/login', { pagina: 'Iniciar Sesión' })
 }
 
-const formularioRegistro = (req,res) =>
-{
-    res.render("auth/registro", {pagina: "Registrate con nosotros :)"});
+const formularioRegistro = (req, res) => {
+    res.render('auth/registro', { pagina: 'Registrarse con nosotros' })
 }
 
-const formularioRecuperacion = (req,res) =>
-{
-    res.render("auth/recuperarPassword", {pagina: "Te ayudamos a restaurar tu contraseña"});
+const formularioOlvidePassword = (req, res) => {
+    res.render('auth/recuperarContraseña', { pagina: 'Recuperar mi contraseña' })
 }
 
-
-export { formularioLogin, formularioRegistro, formularioRecuperacion}
+export { formularioLogin, formularioRegistro, formularioOlvidePassword }
