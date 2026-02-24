0df0e7442064afc1e64b832ad2116bcb1d3ad2f8
Aylin Esteban Luna <240853@utxicotepec.edu.mx>
2026-02-24 08:36:12 -0600

    PC-Bienes Raices - seccion03
    add: Se agrego la seccion 03

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 import express from "express";
-import usuarioRoutes from './routes/usuarioRoutes.js'
+import usuarioRoutes from './routes/usuarioRoutes.js';
 
 // Instanciamos el servidor que alojara la webapp
 const app = express();
 
 // Habilitamos pug
 app.set('view engine', 'pug');
-app.set('views', './views')
+app.set('views', './views');
 
-//Definimos la carpeta publica
-app.use(express.static('public'))
+// Definimos la carpeta de los recursos estáticos
+app.use(express.static('public'));
 
 // Importamos sus rutas (ruteo)
-app.use("/auth",usuarioRoutes)
+app.use("/auth", usuarioRoutes);
 
 const PORT = process.env.PORT ?? 3000;
-app.listen(PORT, ()=> {
-    console.log(`El servidor esta iniciado en el puerto ${PORT}`)
-}) 
+app.listen(PORT, () => {
+    console.log(`El servidor esta iniciado en el puerto ${PORT}`);
+});
